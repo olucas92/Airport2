3cb3a2fae3e341ae717aea80c6ccf48947338b0c
Oliver <o.lucas@live.com>
2015-06-11 12:31:57 +0100

    plane is able to land and test passes

--- lib/Plane.rb
+++ lib/Plane.rb
@@ -6,10 +6,20 @@ class Plane
 
   def fly!
     @flying = true
+    @grounded = false
   end
 
   def flying?
     @flying
+  end
+
+  def grounded?
+    @grounded
+  end
+
+  def land!
+    @flying = false
+    @grounded = true
   end
 
 end
