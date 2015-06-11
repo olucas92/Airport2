72e1cedc3d68613bae72885c966f48854322ef6a
Oliver <o.lucas@live.com>
2015-06-11 12:48:49 +0100

    airport now able to accept and dock planes

--- lib/Airport.rb
+++ lib/Airport.rb
@@ -1,3 +1,21 @@
 class Airport
 
+  attr_reader :planes
+
+  def initialize
+    @planes = []
+  end
+
+  def dock(plane)
+    plane.land!
+    @planes << plane
+  end
+
+  def plane_count
+    @planes.count
+  end
+
+
+
+
 end
