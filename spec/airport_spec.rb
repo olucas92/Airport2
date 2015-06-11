4e2c312b0fefdaaf093ab5ba064c57a9449b3813
Oliver <o.lucas@live.com>
2015-06-11 12:43:36 +0100

    test written for planes being docked into airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,2 +1,25 @@
 require 'airport'
 require 'plane'
+
+describe Airport do
+
+  let(:airport) { Airport.new }
+  let(:plane) { Plane.new }
+
+  context "taking off and landing" do
+
+    it "planes should be able to land" do
+      plane.land!
+      airport.dock(plane)
+      expect(airport.plane_count).to eq(1)
+    end
+
+
+
+
+  
+
+
+
+
+end
