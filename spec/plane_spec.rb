90002e285e6eda8dfdc6967970289a050f69a917
Oliver <o.lucas@live.com>
2015-06-11 12:24:35 +0100

    first test written

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,2 +1,13 @@
 require 'plane'
 require 'airport'
+
+describe Plane do
+
+  it "should have a flying status after being created" do
+    plane = Plane.new
+    plane.fly!
+    expect(plane).to be_flying
+  end
+
+
+
